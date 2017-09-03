6e1d4281e3e6dfdc85ca003532d884e3a53c1136
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-03 22:40:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if(string === love) {
     return "I love you, too."
+  } else if(string === string.toUpperCase())  {
+    return "YES INDEED!"
   }
 }
